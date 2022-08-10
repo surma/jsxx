079aa0476b0df1c801fe716f926ebb98bec30d23
Surma <surma@surma.dev>
2022-08-10 13:58:47 +0100

    Foundation for tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::io::Read;
+use std::{
+    fs::File,
+    io::{Read, Write},
+    process::{Command, Stdio},
+};
 
 use anyhow::{anyhow, Result};
 use swc_common::BytePos;
@@ -7,18 +11,15 @@ use swc_ecma_parser::{lexer::Lexer, EsConfig, Parser, StringInput, Syntax};
 mod globals;
 mod transpiler;
 
-fn main() -> Result<()> {
-    let mut input: String = String::new();
-    std::io::stdin().read_to_string(&mut input)?;
-
+fn js_to_cpp<T: AsRef<str>>(input: T) -> Result<String> {
     let syntax = Syntax::Es(EsConfig::default());
     let lexer = Lexer::new(
         syntax,
         swc_ecma_visit::swc_ecma_ast::EsVersion::Es2022,
         StringInput::new(
-            &input,
+            input.as_ref(),
             swc_common::BytePos(0),
-            BytePos(input.bytes().len().try_into().unwrap()),
+            BytePos(input.as_ref().as_bytes().len().try_into().unwrap()),
         ),
         None,
     );
@@ -30,7 +31,47 @@ fn main() -> Result<()> {
     let mut transpiler = transpiler::Transpiler {
         globals: vec![globals::wasi::WASIGlobal(), globals::json::JSONGlobal()],
     };
-    let result = transpiler.transpile_module(&module)?;
-    println!("{}", result);
+    transpiler.transpile_module(&module)
+}
+
+fn cpp_to_binary<T: AsRef<str>>(code: T, flags: Vec<&str>) -> Result<()> {
+    let mut tempfile = File::create("./generated.cpp")?;
+    tempfile.write_all(code.as_ref().as_bytes())?;
+    tempfile.flush();
+    drop(tempfile);
+
+    let mut child = Command::new("clang++")
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .args(
+            flags
+                .into_iter()
+                .chain(
+                    [
+                        "--std=c++17",
+                        "-o",
+                        "output",
+                        "generated.cpp",
+                        "runtime/global_json.cpp",
+                        "runtime/global_wasi.cpp",
+                        "runtime/js_primitives.cpp",
+                        "runtime/js_value_binding.cpp",
+                        "runtime/js_value.cpp",
+                    ]
+                    .into_iter(),
+                )
+                .collect::<Vec<&str>>(),
+        )
+        .spawn()?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let mut input: String = String::new();
+    std::io::stdin().read_to_string(&mut input)?;
+
+    let cpp_code = js_to_cpp(&input)?;
+    cpp_to_binary(cpp_code, vec![])?;
     Ok(())
 }
