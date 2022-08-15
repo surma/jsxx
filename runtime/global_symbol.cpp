4f33e640c419ad3b74bc68d67625297ac72f3eb6
Surma <surma@surma.dev>
2022-08-16 00:55:48 +0100

    Fix JSON handling of booleans

---
+++ runtime/global_symbol.cpp
@@ -0,0 +1,10 @@
+
+#include "global_json.hpp"
+
+JSValue create_symbol_global() {
+  JSValue global = JSValue::new_object(
+      {{JSValue{"iterator"},
+        JSValueBinding::with_value(JSValue::new_object({}))}});
+
+  return global;
+}
